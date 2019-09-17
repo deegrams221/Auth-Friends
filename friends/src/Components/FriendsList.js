3f1ff72c1cccf030e4f73fb6eb3d5406be24ec5d
Diana Grams <deegrams221@gmail.com>
2019-09-17 16:37:40 -0600

    added FriendForm and set up FriendsList

--- friends/src/Components/FriendsList.js
+++ friends/src/Components/FriendsList.js
@@ -1,26 +1,49 @@
 // In your FriendsList component, rendered with <ProtectedRoute />, you will create a list of your friends that you get from the API.
 // import react, and axiosWithAuth
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {axiosWithAuth} from '../utils/axiosWithAuth';
+import FriendForm from './FriendForm';
 
-class FriendsList extends React.Component {
-  state = {
-    friendsList: []
-  };
+const Friends = () => {
+  const [friends, setFriends] = useState([]);
 
-  componentDidMount() {
-    this.getData();
-  }
-
-  // fetch data from protected api
-  getData = () => {
+  const getFriends = () => {
     axiosWithAuth()
       .get('/friends')
-      .then(res => console.log(res))
-      .catch(error => console.log(error));
+      .then(res => {
+        setFriends(res.data);
+      })
+      .catch(err => console.log(err.response));
   }
-  
-}
 
-export default FriendsList;
+  useEffect(() => {
+    getFriends();
+  }, [])
+
+  const addFriend = friend => {
+    axiosWithAuth()
+      .post('/friends', friend)
+      .then(res => {
+        setFriends(res.data);
+      })
+      .catch(err => console.log(err.response));
+  }
+
+  return (
+    <div>
+      <h1>Add a New Friend</h1>
+      <FriendForm addFriend={addFriend}/>
+      <h2>Friends</h2>
+      {friends.map(friend => 
+        <div className="friend-card" key={friend.id}>
+          <h3>{friend.name}</h3>
+          <h4>Email: {friend.email}</h4>
+          <h5>Age: {friend.age}</h5>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Friends;
